eb84355a741815ec20f7fb16abde991e930645b7
jlarivaux <jplarivaux@gmail.com>
2024-02-18 21:07:05 -0800

    working project

---
+++ app/models/entry.rb
@@ -0,0 +1,7 @@
+class Entry < ApplicationRecord
+    validates :meal_type, :calories, :proteins, :carbohydrates, :fats, presence: true
+
+    def day
+        self.created_at.strftime("%B %e, %Y")   
+    end
+end
